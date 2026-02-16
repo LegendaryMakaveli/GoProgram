ed92375436f03ae07d554c393f9de9d9d631fa94
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2026-02-16 19:19:35 +0100

    validate user input in go

---
+++ validateUserInput/validateUserInput.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var number int
+
+	fmt.Print("Enter a number: ")
+	fmt.Scan(&number)
+
+	for number != 1 && number != 2 {
+		fmt.Println("Invalid number")
+		fmt.Print("Enter a number: ")
+		fmt.Scan(&number)
+	}
+
+	fmt.Println("Valid number")
+}
