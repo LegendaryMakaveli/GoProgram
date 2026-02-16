7bcb40e458b8e71579b1c71d2cfaa42cc93bb979
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2026-02-16 22:01:07 +0100

    updates

--- Chapter4/main.go
+++ Chapter4/main.go
@@ -3,15 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("=== Validate User Input ===")
-	validateUserInput(3)
+	// fmt.Println("=== Validate User Input ===")
+	// validateUserInput(3)
 
-	fmt.Println("\n=== Right Angle ===")
-	rightAngle(5)
+	// fmt.Println("\n=== Right Angle ===")
+	// rightAngle(5)
 
-	fmt.Println("\n=== Largest and Second Largest ===")
-	largestAndSecondLargest(4, 7)
+	// fmt.Println("\n=== Largest and Second Largest ===")
+	// largestAndSecondLargest(4, 7)
 
-	fmt.Println("\n=== Dangling Else Statement ===")
-	danglingElseStatement()
+	// fmt.Println("\n=== Dangling Else Statement ===")
+	// danglingElseStatement()
+
+	fmt.Println("\n=== Palindrome ===")
+	number := 1232
+	fmt.Println(palindrome(number))
+
+
 }
